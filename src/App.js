e8fac2bbaa08d6e0fd3458b4467da98aa28d27f1
EddyBautista <bautista.j.eddy@gmail.com>
2021-07-01 19:14:31 -0500

    testing env

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID={process.env.REACT_APP_SECRET_NAME}
+            userName=""
+            userSecret=""
+            />
+    )
+}
+
+export default App;
