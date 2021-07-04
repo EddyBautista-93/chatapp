6f3da767bd84141600dfd031dbf5b909c2c97417
EddyBautista <bautista.j.eddy@gmail.com>
2021-07-04 09:08:37 -0500

    chat app engine up and running with user stored information in the .env file

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ const App = () => {
         <ChatEngine
             height="100vh"
             projectID={process.env.REACT_APP_SECRET_NAME}
-            userName=""
-            userSecret=""
+            userName={process.env.REACT_APP_SECRET_USER}
+            userSecret={process.env.REACT_APP_SECRET}
             />
     )
 }
-
+ 
 export default App;
